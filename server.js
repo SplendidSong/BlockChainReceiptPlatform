b4c6428b99a0a62cdfcff6063368cce209e61cf2
shenlinsong <442823419@qq.com>
2019-08-06 10:12:32 +0800

    后端写好了

--- server.js
+++ server.js
@@ -1,8 +1,35 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
-app.post('/calculate', function(req, res) {
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Content-Type', 'application/json;charset=utf-8');
+    next();
+});
+
+app.post('/calculate', bodyParser.json() , function(req, res) {
+	var result = 0
+
+	switch(req.body.operator) {
+		case '+' :
+			result = parseFloat(req.body.num1) + parseFloat(req.body.num2)
+			break
+		case '-' :
+			result = parseFloat(req.body.num1) - parseFloat(req.body.num2)
+			break
+		case 'x' :
+			result = parseFloat(req.body.num1) * parseFloat(req.body.num2)
+			break
+		case '/' :
+			result = parseFloat(req.body.num1) / parseFloat(req.body.num2)
+			break
+	}
 	
+	res.send(result)
+
 })
 
 
